4e982ee7c21876fb273649da954778c556320c91
Pavel Zhukov Alekseevich <zhukowgg@gmail.com>
2025-04-22 00:32:08 +0600

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,3 @@
-// import { NavLink } from "react-router-dom";
-// import styles from "./Header.module.scss";
-// import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
-// import { useTranslation } from 'react-i18next';
-// const Header = () => {
-//   const { t } = useTranslation();
-//   return (
-//     <header className={styles.header}>
-//       <div className={styles.logo}>Marvel</div>
-//       <div className={styles.navContainer}><nav>
-//         <NavLink to="/comics">
-//           {t('comicsTitle')}
-//         </NavLink>
-//         <NavLink to="/favorite">
-//           {t('favoritesTitle')}
-//         </NavLink>
-//       </nav></div>
-//       <LanguageSwitcher />
-//     </header>
-//   );
-// };
-
-// export default Header;
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.scss";
 import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
